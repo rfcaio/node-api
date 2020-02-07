2b467804bb6aa0e682f85e6ebfb49eea2e202015
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-07 14:06:04 -0300

    create database settings

---
+++ src/database.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose()
+
+const database = new sqlite3.Database('product.db', error => {
+  console.log(error || 'Database opened.')
+})
+
+database.run(
+  `
+    CREATE TABLE IF NOT EXISTS product (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      created_at TEXT NOT NULL,
+      name TEXT NOT NULL
+    )
+  `,
+  error => error && console.log(error)
+)
+
+module.exports = database
