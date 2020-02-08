d333a2ec581b392ca1fe813ae9c0610bda2bacef
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-08 06:29:48 -0300

    fix product creation

--- src/product/models.js
+++ src/product/models.js
@@ -3,15 +3,12 @@ const database = require('../database')
 const product = {
   create: ({ name }) => {
     const createdAt = new Date().toISOString()
+    const query = 'INSERT INTO product (name, created_at) VALUES (?, ?)'
     return new Promise((resolve, reject) => {
-      database.all(
-        'INSERT INTO product (name, created_at) VALUES (?, ?)',
-        [name, createdAt],
-        (error, products) => {
-          error && reject(new Error('Could not create product.'))
-          resolve('Created with success.')
-        }
-      )
+      database.run(query, [name, createdAt], error => {
+        error && reject(new Error('Could not create product.'))
+        resolve('Created with success.')
+      })
     })
   },
 
