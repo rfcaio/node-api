c75dfac2c4e89852acc1423057839ecb6afd81a2
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-12 13:50:52 -0300

    create user table

--- src/database.js
+++ src/database.js
@@ -15,4 +15,16 @@ database.run(
   error => error && console.log(error)
 )
 
+database.run(
+  `
+    CREATE TABLE IF NOT EXISTS user (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      created_at TEXT NOT NULL,
+      email TEXT NOT NULL,
+      password TEXT NOT NULL
+    )
+  `,
+  error => error && console.log(error)
+)
+
 module.exports = database
