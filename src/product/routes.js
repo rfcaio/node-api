fb76096392c7b51be8eca693ac0d2e13f6a473f9
Caio Rodrigues <rfcaio90@gmail.com>
2020-02-27 13:51:36 -0300

    add validation for product update

--- src/product/routes.js
+++ src/product/routes.js
@@ -48,15 +48,29 @@ routes.delete('/:id', async (req, res) => {
   }
 })
 
-routes.put('/:id', async (req, res) => {
-  const { name } = req.body
-  const { id } = req.params
-  try {
-    await product.update({ id, name })
-    res.status(200).json({ message: 'Updated with success.' })
-  } catch (error) {
-    res.status(500).json({ message: error })
+routes.put(
+  '/:id',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage('Product name should not empty.')
+      .isLength({ min: 6 })
+      .withMessage('Product name must have at least 6 characters.')
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+    const { name } = req.body
+    const { id } = req.params
+    try {
+      await product.update({ id, name })
+      res.status(200).json({ message: 'Updated with success.' })
+    } catch (error) {
+      res.status(500).json({ message: error })
+    }
   }
-})
+)
 
 module.exports = routes
